0e6449fa036bb30557f51d2c9d9c7514021dcaad
Jordi <jordi@shiraz.es>
2019-12-23 11:54:28 +0100

    Module filesystem implemented. Functions tested and working.

---
+++ ModuleFilesystem.h
@@ -0,0 +1,21 @@
+#ifndef __ModuleFilesystem_H__
+#define __ModuleFilesystem_H__
+
+#include "Module.h"
+
+class ModuleFilesystem : public Module
+{
+public:
+	ModuleFilesystem();
+	~ModuleFilesystem();
+
+	bool Load(const char* path, const char* file, char** buffer) const;
+	bool Save(const char* path, const char* file, const char* buffer, unsigned int size, bool append = false) const;
+	bool Remove(const char* file);
+	bool Exists(const char* file) const;
+	bool MakeDirectory(const char* directory);
+	bool IsDirectory(const char* directory) const;
+	bool Copy(const char* source, const char* destination);
+};
+
+#endif __ModuleFilesystem_H__
